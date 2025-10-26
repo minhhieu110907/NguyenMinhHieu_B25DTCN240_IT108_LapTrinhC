a1f70694d8d27fe48b3b72f92e4eeaa47f5aea6c
minhhieu110907 <minhhieu19692016@gmail.com>
2025-10-26 15:33:10 +0700

    Update bai3.c

--- Seassion3/bai3.c
+++ Seassion3/bai3.c
@@ -7,7 +7,10 @@ int main() {
     scanf("%f", &bankinh);
     float dientich = pow(bankinh, 2) * M_PI;
     printf("Dien tich hinh tron la: %.2f\n", dientich);
+    float chuvi = 2 * M_PI * bankinh;
+    printf("Chu vi hinh tron la: %.2f\n",chuvi);
     
 
 return 0;
+
 }
