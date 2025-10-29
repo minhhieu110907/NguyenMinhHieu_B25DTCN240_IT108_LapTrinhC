1accbf0e9c054a11e2f923a6f536da6ebbc2eed4
minhhieu110907 <minhhieu19692016@gmail.com>
2025-10-29 21:19:32 +0700

    update bai9.c
    Removed prime number checking functionality and added matrix input/output.

--- Seassion8/bai9.cpp
+++ Seassion8/bai9.cpp
@@ -1,43 +1,51 @@
 #include<stdio.h>
-#include<math.h>
-int kiemtrasonguyento(int n){
-	switch (n){
-		case 0:
-	    case 1:
-	      return 0;
-	    case 2:
-	      return 1;
-	    default:
-	    	if(n %2 ==0){
-	    		return 0;
-	        }
-	        for(int i=3;i<sqrt(n);i+=2){
-	        	if (n%i==0){
-	        		return 0;
-				} else {
-					return 1;
-				}
-			}	
+int main(){
+  int rows, cols;
+    
+    printf("Nhap so hang: ");
+    scanf("%d", &rows);
+
+    printf("Nhap so cot: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("\n--- Nhap cac phan tu cho ma tran ---\n");
+    
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Nhap phan tu matrix[%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("Hang dau tien trong ma tran la: ");
+    for( int i =0;i<1;i++){
+        for ( int j = 0;j<cols;j++){
+    	   printf("%d ",matrix [i][j]);
 	}
 }
-int main(){
-	int i=0; 
-	int a[i],num;
-	printf("Moi ban nhap vao so phan tu cua mang: ");
-	scanf("%d",&num);
-	
-	for(int i =0;i<num;i++){
-		printf("Moi ban nhap vao phan tu %d cua mang: ",i);
-		scanf("%d",&a[i]);
+    printf("\n");
+    printf("Hang cuoi trong ma tran la: ");
+    for ( int i= rows-1;i<rows;i++){
+    	for(int j=0;j<cols;j++){
+    		printf("%d ",matrix[i][j]);
+		}
 	}
-	printf("So nguyen to trong mang la: ");
-	for(int i =0; i<num;i++){
-		if ( kiemtrasonguyento(i)){
-			printf("%d ",i);
+	printf("\n");
+	printf("Cot dau tien trong ma tran la: ");
+	for( int i =0; i< rows;i++){
+		for ( int j = 0; j< 1; j++ ){
+		printf("%d ",matrix[i][j]);
+	    }
+	}
+	printf("\n");
+	printf("Cot cuoi trong ma tran la: ");
+	for(int i =0; i<rows;i++){
+		for(int j = cols-1;j<cols;j++ ){
+			printf("%d ",matrix[i][j]);
 		}
 	}
 
-
 return 0;
 }
-
