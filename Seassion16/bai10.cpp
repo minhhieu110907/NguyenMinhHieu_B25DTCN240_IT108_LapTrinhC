49ab2fc96f9c2d13b09b58c99a70b063078a148c
minhhieu110907 <minhhieu19692016@gmail.com>
2025-11-13 00:39:28 +0700

    Add files via upload

---
+++ Seassion16/bai10.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX 1000
+int main(){
+   int fre[256]={0},printed[256]={0};
+   char str [MAX];
+   printf("Nhap chuoi: ");
+   fgets(str,MAX,stdin);
+   str[strcspn(str,"\n")]= '\0';
+   
+   printf("Chuoi ban vua nhap la: ");
+   fputs(str,stdout);
+   printf("\n");
+   
+   int len = strlen(str);
+   for ( int i =0;i<len;i++){
+   	  unsigned char charactercurrent = str[i];
+   	  fre[charactercurrent]++;
+   }
+   for ( int i = 0;i<len;i++){
+   	unsigned char character = str[i];
+   	if(fre[character]>0 && printed[character] == 0){
+   		printf("%c: %d\n",character,fre[character]);
+   		printed[character] = 1;
+	   }
+   }
+
+
+return 0;
+}
+
